9faa2078ca6248e0cd9cc983078ddaefe7baa2a6
sagulfrecovery-cmd <sagulf.recovery@gmail.com>
2026-09-16 21:48:09 +0300

    Update portal.py

--- portal.py
+++ portal.py
@@ -19,11 +19,11 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # أوقات يوم الأحد
 SUN_OPEN_HOUR, SUN_OPEN_MIN = 18, 0    # 6:00 PM
-SUN_CLOSE_HOUR, SUN_CLOSE_MIN = 21, 20 # 9:30 PM
+SUN_CLOSE_HOUR, SUN_CLOSE_MIN = 21, 20 # 9:20 PM
 
 # أوقات يوم الأربعاء
 WED_OPEN_HOUR, WED_OPEN_MIN = 18, 0    # 6:00 PM
-WED_CLOSE_HOUR, WED_CLOSE_MIN = 22, 50  # 9:00 PM
+WED_CLOSE_HOUR, WED_CLOSE_MIN = 20, 50  # 8:50 PM
 
 # مدة إغلاق الغرفة بعد بدء الاجتماع (بالدقائق - تستخدم في رسالة التنبيه)
 ROOM_LOCK_MINUTES = 20
