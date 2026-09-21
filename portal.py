72398af1605a0d389d6471b90d823c1df027189d
sagulfrecovery-cmd <sagulf.recovery@gmail.com>
2026-09-21 20:56:11 +0300

    Update portal.py

--- portal.py
+++ portal.py
@@ -72,7 +72,7 @@ st.set_page_config(page_title="بوابة زمالة الخليج", page_icon="�
 baghdad_tz = pytz.timezone("Asia/Baghdad")
 now = datetime.now(baghdad_tz)
 weekday = 2 #now.weekday()  # 6 = الأحد، 2 = الأربعاء
-now_time = now.time()
+now_time = 2 #now.time()
 
 # توليد النصوص آلياً بناءً على الإعدادات أعلاه
 sun_open_str = format_time_arabic(SUN_OPEN_HOUR, SUN_OPEN_MIN)
